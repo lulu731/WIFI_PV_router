a76fa6e16da51106def5962400ecf2b30d2e0cf3
lulu731 <lulu731lulu@hotmail.fr>
2023-03-11 22:51:36 +0100

    Workflow : fix Tests

---
+++ .github/workflows/tests_secrets.h
@@ -0,0 +1,10 @@
+#ifndef __SECRETS_H__
+#define __SECRETS_H__
+
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+
+const char* ap_ssid = "AP_SSID";
+const char* ap_password = "AP_PASSWORD";
+
+#endif
